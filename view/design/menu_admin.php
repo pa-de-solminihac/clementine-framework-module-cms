92ab05db8dbb9b38644a4527288a087b3ec7b5ef
Pierre-Alexis de Solminihac <pa@solminihac.fr>
2013-02-04 01:24:05 +0100

    restoring cms-1.12 branch

--- view/design/menu_admin.php
+++
@@ -1,7 +0,0 @@
-<?php
-$this->getParentBlock($data);
-?>
-                <!-- CMS -->
-                <li>
-                    <li><a href="<?php echo __WWW__; ?>/cms" <?php echo (isset($data['current']) && $data['current'] == "cms") ? 'class="current"' : ''; ?>>Gérer les contenus</a></li>
-                </li>
