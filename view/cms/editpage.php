86ff21c16ccdfe9568af78c9bf5828c78e987003
Pierre-Alexis de Solminihac <pa@quai13.com>
2015-06-23 16:56:36 +0200

    CMS 4.2 fix: new page form

--- view/cms/editpage.php
+++ view/cms/editpage.php
@@ -1,22 +1,20 @@
 <div class="page_edit">
 <?php
-if (isset($data['page']) && $data['page']) {
-    $page = $data['page'];
-} elseif (isset($data['erreurs'])) {
+if (isset($data['erreurs'])) {
     print_r($data['erreurs']);
 }
+$form_url = __WWW__ . '/cms/editpage';
 if (isset($data['page']) && $data['page']) {
+    $form_url.= '?id=' . $data['page']['id'];
+}
 ?>
     <form
         name="edit_page"
         method="post"
         enctype="multipart/form-data"
-        action="<?php echo __WWW__ . '/cms/editpage?id=' . $page['id']; ?>">
+        action="<?php echo $form_url; ?>">
 <?php
     $this->getBlock('cms/editpage_form', $data, $request);
 ?>
     </form>
-<?php
-}
-?>
 </div>
